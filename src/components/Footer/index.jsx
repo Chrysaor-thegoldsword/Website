5fbae6a7afaed94f73bacc515aa4dbe2cff48105
Chrysaor-thegoldsword <45918383+Chrysaor-thegoldsword@users.noreply.github.com>
2025-01-24 17:43:41 +0000

    reverted

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import emailjs from "emailjs-com";
 
-//Niha
 const FooterComponent = () => {
   const form = useRef();
 
